7e109c73f710bef7a1b54d4b9b91d901a9df0089
Iris Tao <mtao@stuy.edu>
2018-05-29 06:07:31 -0400

    stats page stuff

--- lbit/static/js/floor.js
+++ lbit/static/js/floor.js
@@ -1,97 +1,68 @@
-// Set the dimensions of the canvas / graph
-var	margin = {top: 30, right: 20, bottom: 30, left: 50},
-	width = 400 - margin.left - margin.right,
-	height = 220 - margin.top - margin.bottom;
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
 // Parse the date / time
-var	parseDate = d3.time.format("%d-%b-%y").parse;
+var	parseDate = d3.time.format("%Y-%m").parse;
 
-// Set the ranges
-var	x = d3.time.scale().range([0, width]);
-var	y = d3.scale.linear().range([height, 0]);
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
 
-// Define the axes
-var	xAxis = d3.svg.axis().scale(x)
-	.orient("bottom").ticks(5);
+var y = d3.scale.linear().range([height, 0]);
 
-var	yAxis = d3.svg.axis().scale(y)
-	.orient("left").ticks(5);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%Y-%m"));
 
-// Define the line
-var	valueline = d3.svg.line()
-	.x(function(d) { return x(d.date); })
-	.y(function(d) { return y(d.close); });
-    
-// Adds the svg canvas
-var	chart1 = d3.select("body")
-	.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
 
-// Get the data
-d3.csv("../../utils/data/data-1.csv", function(error, data) {
-	data.forEach(function(d) {
-		d.date = parseDate(d.date);
-		d.close = +d.close;
-	});
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
 
-	// Scale the range of the data
-	x.domain(d3.extent(data, function(d) { return d.date; }));
-	y.domain([0, d3.max(data, function(d) { return d.close; })]);
+d3.csv("/static/bar-data.csv", function(error, data) {
 
-	// Add the valueline path.
-	chart1.append("path")
-		.attr("class", "line")
-		.attr("d", valueline(data));
+    data.forEach(function(d) {
+        d.date = parseDate(d.date);
+        d.value = +d.value;
+    });
+	
+  x.domain(data.map(function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-	// Add the X Axis
-	chart1.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
 
-	// Add the Y Axis
-	chart1.append("g")
-		.attr("class", "y axis")
-		.call(yAxis);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Times Broken");
 
-});
-
-// Adds the svg canvas
-var	chart2 = d3.select("body")
-	.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-		
-// Get the data
-d3.csv("../../utils/data/data-2.csv", function(error, data) {
-	data.forEach(function(d) {
-		d.date = parseDate(d.date);
-		d.close = +d.close;
-	});
-
-	// Scale the range of the data
-	x.domain(d3.extent(data, function(d) { return d.date; }));
-	y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-	// Add the valueline path.
-	chart2.append("path")
-		.attr("class", "line")
-		.attr("d", valueline(data));
-
-	// Add the X Axis
-	chart2.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
-
-	// Add the Y Axis
-	chart2.append("g")
-		.attr("class", "y axis")
-		.call(yAxis);
+  svg.selectAll("bar")
+      .data(data)
+    .enter().append("rect")
+      .style("fill", "lightsteelblue")
+      .attr("x", function(d) { return x(d.date); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); });
 
 });
